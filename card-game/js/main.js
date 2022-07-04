5944ec12e07e1453e054b0bb8a68bd4589ed4775
LivCodes <oliviareeddev@gmail.com>
2022-07-04 12:34:39 -0400

    game result

--- card-game/js/main.js
+++ card-game/js/main.js
@@ -25,8 +25,32 @@ function drawTwo() {
       console.log(data);
       document.querySelector("#Player1").src = data.cards[0].image;
       document.querySelector("#Player2").src = data.cards[1].image;
+      let player1Val = convertToNum(data.cards[0].value);
+      let player2Val = convertToNum(data.cards[1].value);
+
+      if (player1Val > player2Val) {
+        document.querySelector("p").innerText = "Player 1 Wins";
+      } else if (player2Val > player1Val) {
+        document.querySelector("p").innerText = "Player 2 Wins";
+      } else {
+        document.querySelector("p").innerText = "Time for War!";
+      }
     })
     .catch((err) => {
       console.log(`error ${err}`);
     });
+}
+
+function convertToNum(val) {
+  if (val === "ACE") {
+    return 14;
+  } else if (val === "KING") {
+    return 13;
+  } else if (val === "QUEEN") {
+    return 12;
+  } else if (val === "JACK") {
+    return 11;
+  } else {
+    return Number(val);
+  }
 }
