d711d9862e6505b802bdd158ea1c7ead53d15575
LivCodes <oliviareeddev@gmail.com>
2022-07-04 12:02:23 -0400

    fetch draws two cards

--- card-game/js/main.js
+++ card-game/js/main.js
@@ -1,17 +1,32 @@
-//Example fetch using pokemonapi.co
-document.querySelector('button').addEventListener('click', getFetch)
+let deckId = "";
 
-function getFetch(){
-  const choice = document.querySelector('input').value
-  const url = 'https://pokeapi.co/api/v2/pokemon/'+choice
+fetch("https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1")
+  .then((res) => res.json()) // parse response as JSON
+  .then((data) => {
+    if (!localStorage.getItem("deck_id")) {
+      localStorage.setItem("deck_id", data.deck_id);
+      deckId = localStorage.getItem("deck_id");
+    } else {
+      deckId = localStorage.getItem("deck_id");
+    }
+  })
+  .catch((err) => {
+    console.log(`error ${err}`);
+  });
+
+document.querySelector("button").addEventListener("click", drawTwo);
+
+function drawTwo() {
+  const url = `https://deckofcardsapi.com/api/deck/${deckId}/draw/?count=2`;
 
   fetch(url)
-      .then(res => res.json()) // parse response as JSON
-      .then(data => {
-        console.log(data)
-      })
-      .catch(err => {
-          console.log(`error ${err}`)
-      });
+    .then((res) => res.json()) // parse response as JSON
+    .then((data) => {
+      console.log(data);
+      document.querySelector("#Player1").src = data.cards[0].image;
+      document.querySelector("#Player2").src = data.cards[1].image;
+    })
+    .catch((err) => {
+      console.log(`error ${err}`);
+    });
 }
-
